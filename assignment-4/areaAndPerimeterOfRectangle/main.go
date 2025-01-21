d6d96f5f5871a976f7551038acb200647a817242
adityapadekar-josh <aditya.padekar@joshsoftware.com>
2025-01-21 15:25:08 +0530

    add assignment four: structs, methods and interfaces (#4)
    * add assignment four: structs, methods and interfaces
    
    * handle error gracefully and correct typo
    
    * handle error gracefully and correct typo
    
    * bug fix

---
+++ assignment-4/areaAndPerimeterOfRectangle/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Rectangle struct {
+	length int
+	width  int
+}
+
+func NewRectangle(length, width int) Rectangle {
+	return Rectangle{
+		length: length,
+		width:  width,
+	}
+}
+
+func (r Rectangle) Area() int {
+	return r.length * r.width
+}
+
+func (r Rectangle) Perimeter() int {
+	return 2 * (r.length + r.width)
+}
+
+func main() {
+	var inputLength, inputWidth int
+
+	if _, err := fmt.Scan(&inputLength, &inputWidth); err != nil {
+		fmt.Println("Invalid  input")
+		return
+	}
+
+	rectangle := NewRectangle(inputLength, inputWidth)
+
+	fmt.Println("Area =", rectangle.Area())
+	fmt.Println("Perimeter =", rectangle.Perimeter())
+}
